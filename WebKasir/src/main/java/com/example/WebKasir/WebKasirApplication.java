db892ab57f02d3b515b6feb66cba14f358c5d2f4
Refiando535180098 <refiando.535180098@stu.untar.ac.id>
2022-05-10 21:50:04 +0700

    Tugas_JavaSpring
    Project yang sudah di ubah kedalam framework Java Spring

---
+++ WebKasir/src/main/java/com/example/WebKasir/WebKasirApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebKasir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebKasirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebKasirApplication.class, args);
+	}
+
+}
---
+++ WebKasir/src/test/java/com/example/WebKasir/WebKasirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebKasir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebKasirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
